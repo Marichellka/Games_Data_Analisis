5b7023b0c2082cdb92a932fac1649e993bc601b3
Marichka <marichka.v.richtsi@gmail.com>
2022-05-21 12:51:07 +0200

    add fill_na method

--- scripts/cleansing.py
+++ scripts/cleansing.py
@@ -1,2 +1,16 @@
 import pandas as pd
 
+def read_data(path, *args):
+    dataFrame = pd.read_csv(path, *args)
+    return dataFrame
+
+
+def fill_NaN_with_mean(dataFrame, columns):
+    dataFrame[columns]= dataFrame[columns].fillna(dataFrame[columns].mean())
+    return dataFrame
+
+
+df = read_data('data/game_analysis.csv')
+df = fill_NaN_with_mean(df, ['averageplaytime'])
+print(df.info(), '\n')
+print(str(df.head()))
