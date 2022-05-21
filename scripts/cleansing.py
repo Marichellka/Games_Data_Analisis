15a0f1f07ef3d933dc7bf941b6bc3ba3c3c14b82
Nikita Kovalenko <nikkv.ottf@gmail.com>
2022-05-21 14:43:03 +0200

    added vgsales & config

--- scripts/cleansing.py
+++ scripts/cleansing.py
@@ -1,16 +1,19 @@
 import pandas as pd
 from pandas import DataFrame
 
-def read_and_cleanse(dataset_path : str, \
-    mean_columns : list = [], \
+def read_and_cleanse(dataset_path : str,
+    delete_columns : list = [],
+    mean_columns : list = [],
     mode_columns : list = [],
     **kwargs):
     dataset = pd.read_csv(dataset_path, **kwargs)
+    for column in delete_columns:
+        dataset = dataset.drop(column, axis=1)
     cleanse_data(dataset, mean_columns, mode_columns)
     return dataset
 
-def cleanse_data(dataset : DataFrame, \
-    mean_columns : list = [], \
+def cleanse_data(dataset : DataFrame,
+    mean_columns : list = [],
     mode_columns : list = []):
     for column in mean_columns:
         replace_with_mean(dataset, column)
