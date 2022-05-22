664c32024eade093b7a5c9274995dfdc59d2e482
Marichka <marichka.v.richtsi@gmail.com>
2022-05-22 22:57:32 +0200

    add new data with regions

--- scripts/cleansing.py
+++ scripts/cleansing.py
@@ -12,6 +12,7 @@ def read_and_cleanse(dataset_path : str,
     cleanse_data(dataset, mean_columns, mode_columns)
     return dataset
 
+
 def cleanse_data(dataset : DataFrame,
     mean_columns : list = [],
     mode_columns : list = []):
@@ -20,9 +21,37 @@ def cleanse_data(dataset : DataFrame,
     for column in mode_columns:
         replace_with_mode(dataset, column)
 
+
 def replace_with_mean(dataset : DataFrame):
     dataset.fillna(dataset.mean(numeric_only=True), inplace=True)
+
 
 def replace_with_mode(dataset : DataFrame):
     for column in dataset.columns:
         dataset[column].fillna(dataset[column].mode()[0], inplace=True)
+
+
+def refactor_data(dataset: DataFrame):
+    regions = {'NA_Sales':'North America', 'EU_Sales':'Europe', 
+    'JP_Sales':'Japan', 'Other_Sales':'Other'}
+    new_dataset = pd.DataFrame(columns = [])
+    for ind in range(len(dataset)):
+        row = dataset.iloc[ind:].head(1)
+        for key, value in regions.items():
+            new_row = row[['Name','Platform','Year','Genre','Publisher']]
+            new_row = insert_columns(new_row, columns=['Region', 'Region_Sales', 'Global_Sales'],
+                                    values=[[value], row[[key]].values[0], row[['Global_Sales']].values[0]])
+            new_dataset = pd.concat([new_dataset, new_row])
+    return new_dataset
+
+
+def insert_columns(row, columns, values):
+    for index in range(len(row.columns), \
+        len(row.columns)+len(columns)):
+        row.insert(index, columns[index], values[index])
+    return row
+        
+
+dataset = read_and_cleanse("data/vgsales.csv", delete_columns=['Rank'])
+refactor_data(dataset).to_csv('data/sales.csv')
+
