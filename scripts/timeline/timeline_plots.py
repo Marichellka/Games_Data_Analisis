2eebb99ec4f4f4a720e61b2ba913ccff42f0022b
Nikita Kovalenko <nikkv.ottf@gmail.com>
2022-05-25 00:29:13 +0200

    timeline folder & plot_timeline method.

---
+++ scripts/timeline/timeline_plots.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+from pandas import DataFrame
+
+def plot_timeline(
+    dataset : DataFrame,
+    date_column : str,
+    value_column : str,
+    label : str = "",
+    windows : list = []):
+    series = dataset.sort_values([date_column]).groupby(date_column)[value_column].sum()
+    rolling_means = [(series.rolling(window=n).mean(), n) for n in windows]
+    for rolling_mean, window in rolling_means:
+        plt.plot(rolling_mean, label=f"Rolling mean trend, n = {window}")
+    plt.plot(series, label=label)
+    plt.legend(loc="upper left")
+    plt.grid(True)
+    plt.show()
