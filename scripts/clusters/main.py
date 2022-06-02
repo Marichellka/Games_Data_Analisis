dd93396844996af8fc2e5b6fb92346f9c82d6f85
Marichka <marichka.v.richtsi@gmail.com>
2022-06-02 21:29:44 +0200

    separate regression and clustering

---
+++ scripts/clusters/main.py
@@ -0,0 +1,5 @@
+from pandas import DataFrame
+
+
+def clustering(dataset: DataFrame):
+    pass
--- scripts/main.py
+++ scripts/main.py
@@ -1,17 +1,12 @@
 from scripts.cleansing import read_and_cleanse
 from scripts.config import DATA_PATH_SALES
-from scripts.utils.dataset_scaler import DatasetScaler
-from scripts.helpers import delete_useless_elements
-from scripts.regressions.helpers import get_regression_model
+from scripts.regressions.main import regression_analysis
+from scripts.clusters.main import clustering
 
 #TODO: create web API
 
 dataset = read_and_cleanse(DATA_PATH_SALES, mode_columns=["Year"])
 
-x_cols = delete_useless_elements(list(dataset.columns), 
-    useless_elements=["Name", "Publisher", "Global_Sales", "Region_Sales"])
-y_cols = ["Region_Sales"]
+regression_analysis(dataset)
 
-dataset_scaler = DatasetScaler(dataset, x_cols)
-
-regression_model, score = get_regression_model(dataset_scaler.scaled_dataset, x_cols, y_cols)
+clustering(dataset)
