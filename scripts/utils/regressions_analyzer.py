07143389d9496d65b492e3f54e3b8e0f97ab616a
Nikita Kovalenko <nikkv.ottf@gmail.com>
2022-06-14 01:04:35 +0200

    fixed regression score calculation

--- scripts/utils/regressions_analyzer.py
+++ scripts/utils/regressions_analyzer.py
@@ -23,45 +23,39 @@ class RegressionsAnalyzer:
 
 
     def run(self) -> None:
-        x_cols_combinations = {}
-        idx = 0
+        x_cols_combinations = []
         for length in range(1, len(self.__x_cols) + 1):
             for combination in combinations(self.__x_cols, length):
-                x_cols_combinations[idx] = list(combination)
-                idx += 1
+                x_cols_combinations.append(list(combination))
+
         self.__x_cols_combinations = x_cols_combinations
 
-        regressions_lookup = {}
-        for idx, regression in enumerate(self.__regressions):
-            regressions_lookup[idx] = regression
-        self.__regressions_lookup = regressions_lookup
-
-        regressions_scores = {}
-        
-        for idx, combination in self.__x_cols_combinations.items():
-            regressions_scores[idx] = self.get_regressions_scores(combination)
+        regressions_scores = self.get_regressions_scores(self.__x_cols_combinations)
 
         self.__regressions_scores = regressions_scores
 
 
-    def get_regressions_scores(self, x_cols : list) -> dict:
-        x_train, y_train, x_test, y_test = \
-            train_test_dataset_split(self.__dataset, x_cols, self.__y_cols)
+    def get_regressions_scores(self, x_cols_list : list) -> dict:
+        regressions_tests = []
+        for x_cols in x_cols_list:
+            x_train, y_train, x_test, y_test = \
+                train_test_dataset_split(self.__dataset, x_cols, self.__y_cols, shuffle=False)
         
-        fit_regressions = [regression.fit(x_train, y_train) \
-            for regression in self.__regressions]
+            fit_regressions = [regression.fit(x_train, y_train) \
+                for regression in self.__regressions]
 
-        regressions_tests = list(test_regressions(fit_regressions, x_test, y_test))
+            regressions_tests += list(test_regressions(fit_regressions, x_test, y_test))
 
         beneficial_funcs = [
-            lambda x: x, # mean squared error
-            lambda x: [value + abs(min(x)) for value in x] # r2_score
+            lambda x: [max(x) - value for value in x], # mean squared error
+            lambda x: [value + abs(min(map(lambda el : el - 1, x))) - 1 for value in x] # r2_score
         ]
-
-        regressions_scores = {}
-
-        for idx, score in enumerate(self.evaluate_regressions_tests(regressions_tests, beneficial_funcs)):
-            regressions_scores[idx] = score
+        raw_regressions_scores = self.evaluate_regressions_tests(regressions_tests, beneficial_funcs)
+        regressions_scores = []
+        
+        regressions_count = len(self.__regressions)
+        for idx in range(0, len(x_cols_list)):
+            regressions_scores.append(raw_regressions_scores[regressions_count*idx:regressions_count*idx+regressions_count])
 
         return regressions_scores
 
@@ -77,15 +71,15 @@ class RegressionsAnalyzer:
                 for criterions in beneficial_criterions_lists]
         # worst == 0, best == number of criterions in regressions_tests
         regressions_scores = \
-            [sum([value / max_criterions_list[idx] for idx, value in enumerate(test)]) \
-                for test in regressions_tests]
+            [sum([beneficial_criterions_lists[i][j] / max_criterions_list[i] for i, _ in enumerate(test)]) \
+                for j, test in enumerate(regressions_tests)]
         return regressions_scores
 
 
     def dump_scores(self, filename : str) -> None:
         with open(filename, "w") as text_file:
-            for i, regressions_scores in self.__regressions_scores.items():
-                for j, score in regressions_scores.items():
-                    text_file.write(f"Combination: {self.__x_cols_combinations[i]}, ")
-                    text_file.write(f"Regression: {self.__regressions_lookup[j]}, ")
+            for i, regressions_scores in enumerate(self.__regressions_scores):
+                for j, score in enumerate(regressions_scores):
+                    text_file.write(f"Combination: {self.__x_cols_combinations[i % len(self.__x_cols_combinations)]}, ")
+                    text_file.write(f"Regression: {self.__regressions[j]}, ")
                     text_file.write(f"Score: {score}\n")
